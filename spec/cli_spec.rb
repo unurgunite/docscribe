b7f6314357e0cc6b8da7cf7198f20a75e2cbfecb
unurgunite <senpaiguru1488@gmail.com>
2026-01-21 17:37:57 +0300

    [1.1.1] Updated specs

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -8,9 +8,11 @@ RSpec.describe 'CLI docscribe' do
     code = <<~RUBY
       class D; def x; 1; end; end
     RUBY
-    stdout, status = Open3.capture2('ruby', exe, '--stdin', stdin_data: code)
-    expect(status.success?).to be true
-    expect(stdout).to include('# +D#x+')
+    Dir.mktmpdir do |dir|
+      stdout, status = Open3.capture2('ruby', exe, '--stdin', stdin_data: code, chdir: dir)
+      expect(status.success?).to be true
+      expect(stdout).to include('# +D#x+')
+    end
   end
 
   it 'respects emit.return_tag override in YAML' do
