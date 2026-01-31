56526313e9469123618d5d02092120957100c4a1
unurgunite <senpaiguru1488@gmail.com>
2026-02-01 00:42:08 +0300

    [1.2.0] Updated docs

--- spec/module_function_spec.rb
+++ spec/module_function_spec.rb
@@ -2,7 +2,8 @@
 
 RSpec.describe 'module_function handling' do
   def inline(code)
-    Docscribe::InlineRewriter.insert_comments(code, config: Docscribe::Config.new({}))
+    conf = Docscribe::Config.new({})
+    Docscribe::InlineRewriter.insert_comments(code, config: conf)
   end
 
   it 'documents methods after `module_function` (no args) as module methods' do
@@ -19,7 +20,7 @@ RSpec.describe 'module_function handling' do
     expect(out).to include('# @return [Integer]')
   end
 
-  it 'retroactively documents `module_function :foo` methods as module methods' do
+  it 'retroactively documents `module_function :foo` as a module method' do
     code = <<~RUBY
       module M
         def foo; 1; end
@@ -32,5 +33,20 @@ RSpec.describe 'module_function handling' do
 
     expect(out).to include('# +M.foo+')
     expect(out).to include('# +M#bar+')
+  end
+
+  it 'handles multiple names in `module_function :foo, :bar`' do
+    code = <<~RUBY
+      module M
+        def foo; 1; end
+        def bar; 2; end
+        module_function :foo, :bar
+      end
+    RUBY
+
+    out = inline(code)
+
+    expect(out).to include('# +M.foo+')
+    expect(out).to include('# +M.bar+')
   end
 end
