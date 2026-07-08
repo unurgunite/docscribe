5009370778def1c1dd07be55617c0b3e2be1301c
unurgunite <senpaiguru1488@gmail.com>
2026-07-09 00:58:27 +0300

    [1.5.2] Fix rubocop metrics offenses in server.rb

--- lib/docscribe/server.rb
+++ lib/docscribe/server.rb
@@ -481,7 +481,7 @@ module Docscribe
       # @return [void]
       # @return [Object] if Docscribe::ParseError
       # @return [Object] if StandardError
-      def handle_check(client, id, params)
+      def handle_check(client, id, params) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength
         file = params['file']
         strategy = (params['strategy'] || 'safe').to_sym
         return send_error(client, id, -32_602, "File not found: #{file}") unless file && File.file?(file)
@@ -507,7 +507,7 @@ module Docscribe
       # @return [void]
       # @return [Object] if Docscribe::ParseError
       # @return [Object] if StandardError
-      def handle_fix(client, id, params)
+      def handle_fix(client, id, params) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength
         file = params['file']
         strategy = (params['strategy'] || 'safe').to_sym
         return send_error(client, id, -32_602, "File not found: #{file}") unless file && File.file?(file)
@@ -609,7 +609,6 @@ module Docscribe
         client.write(Protocol.serialize(response))
       end
 
-
       # Classify an exception into a standardized error code, message, and data.
       #
       # @private
@@ -617,7 +616,7 @@ module Docscribe
       # @param [nil] _method_name JSON-RPC method name (unused, for future use)
       # @param [Hash] params request params for context
       # @return [Array]
-      def classify_error(exception, _method_name = nil, params = {}) # rubocop:disable Metrics/CyclomaticComplexity,Metrics/MethodLength
+      def classify_error(exception, _method_name = nil, params = {}) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
         # steep:ignore:start
         if exception.is_a?(LoadError) || exception.is_a?(Gem::LoadError)
           data = {}
