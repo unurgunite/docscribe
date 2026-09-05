75bf5ceefbb971e8cb74778e4d12f7086d86e58d
unurgunite <senpaiguru1488@gmail.com>
2026-09-05 13:25:52 +0300

    [1.6.2] Updated RuboCop

--- lib/docscribe/inline_rewriter.rb
+++ lib/docscribe/inline_rewriter.rb
@@ -154,7 +154,7 @@ module Docscribe
 
       # Setup rewrite env
       #
-      # @private1
+      # @private
       # @param [String] code the Ruby source code string to parse and rewrite
       # @param [Hash<Symbol, Object>] options hash containing :config, :file, and :core_rbs_provider
       # @raise [Docscribe::ParseError]
